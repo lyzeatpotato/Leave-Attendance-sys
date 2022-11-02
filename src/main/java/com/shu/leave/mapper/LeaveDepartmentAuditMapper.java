95232a5c2eab29f01c53c85efc479de42323a0ec
Zs89126 <747230633@qq.com>
2022-11-02 10:52:19 +0800

    11.2冲突解决

---
+++ src/main/java/com/shu/leave/mapper/LeaveDepartmentAuditMapper.java
@@ -0,0 +1,62 @@
+package com.shu.leave.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.shu.leave.entity.LeaveDepartmentAudit;
+import org.apache.ibatis.annotations.*;
+
+import java.util.Date;
+
+@Mapper
+public interface LeaveDepartmentAuditMapper extends BaseMapper<LeaveDepartmentAudit> {
+    //部门人事审核
+    @Insert({
+            "insert into leave_department_audit",
+            "(formid,dp_officer_id,dp_officer_result,dp_officer_recommend,dp_officer_time,dp_officer_status,dp_leader_id,dp_leader_result,dp_leader_recommend,dp_leader_time,dp_leader_status,is_deleted,gmt_create,gmt_modified)",
+            "values (#{formId,jdbcType=BIGINT},#{dpOfficerId,jdbcType=VARCHAR},#{dpOfficerResult,jdbcType=VARCHAR},#{dpOfficerRecommend,jdbcType=VARCHAR}, #{dpOfficerTime,jdbcType=TIMESTAMP},#{dpOfficerStatus,jdbcType=CHAR},",
+            "#{dpLeaderId,jdbcType=VARCHAR},#{dpLeaderResult,jdbcType=VARCHAR},#{dpLeaderRecommend,jdbcType=VARCHAR}, #{dpLeaderTime,jdbcType=TIMESTAMP},#{dpLeaderStatus,jdbcType=CHAR},",
+            "#{isDeleted,jdbcType=CHAR}, #{gmtCreate,jdbcType=TIMESTAMP}, #{gmtModified,jdbcType=TIMESTAMP})"
+    })
+    @SelectKey(statement = "SELECT SCOPE_IDENTITY();", keyProperty = "id", before = false, resultType = Long.class)
+    void addLeaveDepartmentAudit(LeaveDepartmentAudit leaveDepartmentAudit);
+
+    //部门负责人审核
+    @Update({
+            "update leave_department_audit",
+            "set dp_leader_id=#{userid,jdbcType=VARCHAR},dp_leader_result=#{result,jdbcType=VARCHAR},dp_leader_recommend=#{recommend,jdbcType=VARCHAR},dp_leader_time=#{time,jdbcType=TIMESTAMP},dp_leader_status='1',gmt_modified=#{time,jdbcType=TIMESTAMP}",
+            "where formid=#{id,jdbcType=BIGINT}"
+    })
+    void updateLeaveDepartmentAudit(String userid,Long id,String result,String recommend,Date time);
+
+    /*
+    部门人事审核通过与否对应得到leave表的修改
+     */
+    @Update({
+            "update leave",
+            "set status='0',department_status='3',hr_status='0',school_status='0',gmt_modified=#{time,jdbcType=TIMESTAMP}",
+            "where id=#{id,jdbcType=BIGINT}"
+    })
+    void dpOfficerAudity(Long id,Date time);
+    @Update({
+            "update leave",
+            "set status='2',department_status='1',hr_status='2',school_status='2',gmt_modified=#{time,jdbcType=TIMESTAMP}",
+            "where id=#{id,jdbcType=BIGINT}"
+    })
+    void dpOfficerAuditn(Long id,Date time);
+
+    /*
+    部门负责人审核通过与否对应得到leave表的修改
+     */
+    @Update({
+            "update leave",
+            "set status='0',department_status='1',hr_status='0',school_status='0',gmt_modified=#{time,jdbcType=TIMESTAMP}",
+            "where id=#{id,jdbcType=BIGINT}"
+    })
+    void dpLeaderAudity(Long id,Date time);
+    @Update({
+            "update leave",
+            "set status='2',department_status='1',hr_status='2',school_status='2',gmt_modified=#{time,jdbcType=TIMESTAMP}",
+            "where id=#{id,jdbcType=BIGINT}"
+    })
+    void dpLeaderAuditn(Long id,Date time);
+}
