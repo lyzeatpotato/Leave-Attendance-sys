2565923c82e03cf3aeff1874a01bf1399c8bdcd7
Zs89126 <747230633@qq.com>
2022-11-14 17:08:22 +0800

    11.14  审核端口修改

--- src/main/java/com/shu/leave/service/AuditService.java
+++ src/main/java/com/shu/leave/service/AuditService.java
@@ -2,9 +2,11 @@ package com.shu.leave.service;
 
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
 public interface AuditService {
-
+    @Transactional
     int singleLeaveAudit(String role, String userid, Long id, String result, String recommend);
 }
